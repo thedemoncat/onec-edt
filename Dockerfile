eea2fb1055b04b80954fbc3bbea4674a170641b0
ZhdanovR <nl.ruslan@yandex.ru>
2021-03-22 18:17:07 +0600

    - oneget 0.2.1 - github action

--- Dockerfile
+++ Dockerfile
@@ -6,37 +6,35 @@ LABEL maintainer="Ruslan Zhdanov <nl.ruslan@yandex.ru> (@TheDemonCat)"
 ARG ONEC_USERNAME
 ARG ONEC_PASSWORD
 ARG VERSION
-ENV installer_type=edt
+ARG ONEGET_VER=v0.1.10
+ARG EDT_VERSION=latest
 
-RUN apk --no-cache add bash curl grep \
-  && mkdir /edt \
-  && cd /edt \
-  && curl -O "https://raw.githubusercontent.com/TheDemonCat/onec_downloader/master/download.sh" \
-  && chmod +x download.sh \
-  && sync; ./download.sh \
-  && for file in *.tar.gz; do tar -zxf "$file"; done \
-  && rm -rf *.tar.gz
+RUN apk add curl bash\
+  && cd /tmp \
+  && curl -sL http://git.io/oneget.sh > oneget \
+  && chmod +x oneget \ 
+  && ./oneget edt:deb@$EDT_VERSION --extract --rename
 
-FROM demoncat/onec-full as base
+FROM ghcr.io/thedemoncat/onec-client:$ONEC_VERSION as base
 
 LABEL maintainer="Ruslan Zhdanov <nl.ruslan@yandex.ru> (@TheDemonCat)"
 
-COPY --from=downloader /edt /edt/
+# COPY --from=downloader /edt /edt/
 
-RUN  apt-get update && \
-  apt install -y openjdk-11-jdk && \
-  rm -rf /var/lib/apt/lists/* && \
-  rm -rf /var/cache/oracle-jdk8-installer
+# RUN  apt-get update && \
+#   apt install -y openjdk-11-jdk && \
+#   rm -rf /var/lib/apt/lists/* && \
+#   rm -rf /var/cache/oracle-jdk8-installer
 
-RUN cd /edt \ 
-    && sync; ./1ce-installer-cli install  \
-    && cd .. \
-    && rm -rf /edt
+# RUN cd /edt \ 
+#     && sync; ./1ce-installer-cli install  \
+#     && cd .. \
+#     && rm -rf /edt
 
-RUN mkdir -p /root/.1cv8/1C/1cv8/conf/
+# RUN mkdir -p /root/.1cv8/1C/1cv8/conf/
 
-ENV PATH /opt/1C/1CE/components/1c-enterprise-ring-0.11.8+4-x86_64:$PATH
+# ENV PATH /opt/1C/1CE/components/1c-enterprise-ring-0.11.8+4-x86_64:$PATH
 
-ENV RING_OPTS="-Duser.country=ru -Duser.language=RU"
+# ENV RING_OPTS="-Duser.country=ru -Duser.language=RU"
 
-USER usr1cv8 
+# USER usr1cv8 
--- make.sh
+++ make.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
-set -e
+set -xe
 
-FILE=.env
-if [[ -f "$FILE" ]]; then
-    export $(grep -v '^#' "$FILE" | xargs)
+export $(grep -v '^#' .env | xargs)
+
+if [ ! -f .env ]; then
+    echo "Please, create .env file"
+    exit 1
 fi
 
-docker build -t demoncat/onec-full \
-    --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
-    --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
-    --build-arg VERSION="$ONEC_VERSION" \
-    https://github.com/TheDemonCat/onec-full.git
+if [ -z "$HASP_SERVER" ]; then
+  HASP_SERVER=localhost
+fi
 
-docker build -t demoncat/edt \
-    --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
-    --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
-    --build-arg ONEC_VERSION="$ONEC_VERSION"  \
-    --build-arg VERSION="$VERSION_EDT" .
+IMAGE_NAME=${1:-"ghcr.io/thedemoncat/onec-edt"}
+
+cp onec-client/config/nethasp.ini onec-client/nethasp.ini
+sed -i "s/"%HASP_SERVER%"/$HASP_SERVER/" onec-client/nethasp.ini
+
+env=()
+while IFS= read -r line || [[ "$line" ]]; do
+  env+=("$line")
+done < ONEC_VERSION
+
+for ONEC_VERSION in ${env[*]}
+do
+    # docker build -t  ghcr.io/thedemoncat/onec-full:"$ONEC_VERSION" \
+    #     -f onec-client/onec-full/Dockerfile \
+    #     --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+    #     --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+    #     --build-arg ONEC_VERSION="$ONEC_VERSION" onec-client/onec-full
+
+    # docker build -t ghcr.io/thedemoncat/onec-client:"$ONEC_VERSION" \
+    #     --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+    #     --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+    #     --build-arg ONEC_VERSION="$ONEC_VERSION" onec-client
+    
+    docker build -t "$IMAGE_NAME":"$ONEC_VERSION" \
+        --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
+        --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
+        --build-arg ONEC_VERSION="$ONEC_VERSION" .
+
+done
+
+rm -f onec-client/nethasp.ini
